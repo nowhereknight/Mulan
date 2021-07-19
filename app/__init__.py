397adf8c65edf34537998bfa5c5334c8b3270556
ZurielRod <zuriel.rodriguez@storicard.com>
2021-07-19 11:27:23 -0500

    heroku changes

--- app/__init__.py
+++ app/__init__.py
@@ -20,19 +20,25 @@ nyse_symbols = pdr.get_nasdaq_symbols()
 nyse_symbols = nyse_symbols.index.values
 
 if not app.debug:
-    # ...
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/mulan.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/mulan.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info('Mulan startup')
+    app.logger.info('Microblog startup')
+
 
 
 
